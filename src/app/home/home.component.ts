04cf720170b90c10c323f3cf303882200f4cd231
Kiran Joshi <Kiran.Joshi@Agilisys.co.uk>
2018-10-17 18:20:18 +0530

    BC Analytics : Navbar added with Angular material icon

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,OnDestroy ,ChangeDetectorRef,ViewEncapsulation} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../auth/auth.service';
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,OnDestroy{
 
-  constructor(private router: Router, private authService: AuthService) { }
-
-  ngOnInit() {
-  }
+  constructor(private router: Router, private authService: AuthService,changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 1920px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+   }
 
   signOut(){
     this.authService.signOut();
     this.router.navigate(['/login']);
+  }
+
+
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  ngOnInit(): void {
   }
 
 }
