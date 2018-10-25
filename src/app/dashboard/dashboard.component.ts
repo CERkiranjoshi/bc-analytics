89b3306b5c44e0de6be2444394491ae7783e0338
Kiran Joshi <Kiran.Joshi@Agilisys.co.uk>
2018-10-25 16:55:20 +0530

    BC Analytics : Content panel added.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,39 +2,51 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import * as _ from 'lodash';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class DashboardComponent implements OnInit {
 
-  contentFilters: any = {};
-  tempContent: any = {};
+    contentFilters: any = {};
+    tempContent: any = {};
+    panelOpenState = true;
 
-  constructor() {
-    this.contentFilters.allOfThese = [];
-    this.contentFilters.atLeastOnce = [];
-    this.contentFilters.noneOfThese = [];
-    this.tempContent.allOfThese = '';
-    this.tempContent.atLeastOnce = '';
-    this.tempContent.noneOfThese = '';
-  }
-
-  ngOnInit() {
-  }
-  onEnter(val: string) {
-    let searchIndex = _.indexOf(this.contentFilters[val],this.tempContent[val].trim());
-    if (this.tempContent[val].trim()!='' && this.tempContent[val].trim().length > 2 && searchIndex === -1) {
-      this.contentFilters[val].push(this.tempContent[val].trim());
-      this.tempContent[val] = '';
+    constructor() {
+        this.initFilter();
     }
-  }
+
+    ngOnInit() {
+    }
+
+    initFilter() {
+        this.contentFilters.allOfThese = [];
+        this.contentFilters.atLeastOnce = [];
+        this.contentFilters.noneOfThese = [];
+        this.tempContent.allOfThese = '';
+        this.tempContent.atLeastOnce = '';
+        this.tempContent.noneOfThese = '';
+    }
+
+    onEnter(val: string) {
+        let searchIndex = _.indexOf(this.contentFilters[val], this.tempContent[val].trim());
+        if (this.tempContent[val].trim() != '' && this.tempContent[val].trim().length > 2 && searchIndex === -1) {
+            this.contentFilters[val].push(this.tempContent[val].trim());
+            this.tempContent[val] = '';
+        }
+    }
 
 
-  removeText(val,i){
-      this.contentFilters[val].splice(i, 1);
-      console.log(this.contentFilters);
-  }
+    removeText(val, i) {
+        this.contentFilters[val].splice(i, 1);
+        console.log(this.contentFilters);
+    }
+
+    applyFilter() {
+        if (this.contentFilters.allOfThese.length === 0 && this.contentFilters.atLeastOnce.length === 0 && this.contentFilters.noneOfThese.length === 0) {
+            return false;
+        }
+    }
 
 }
